191495f835ed3fd4bf517db41c3b801781c30070
Isnard Silva <isnardsilva@icloud.com>
2020-11-07 00:07:36 -0300

    Setup TableView

--- Movieflix/Controller/MovieListViewController.swift
+++ Movieflix/Controller/MovieListViewController.swift
@@ -8,14 +8,48 @@
 import UIKit
 
 class MovieListViewController: UIViewController {
-
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - Properties
+    private var movies: [Movie] = []
     
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Setup Table View
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
     }
 
 
+}
+
+
+// MARK: - UITableViewDataSource
+extension MovieListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Identifier.Cell.movieCell, for: indexPath)
+        cell.textLabel?.text = "abc"
+        return cell
+    }
+    
+    
+}
+
+
+// MARK: - UITableViewDelegate
+extension MovieListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("didSelectRowAt")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
---
+++ Movieflix/Model/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  Movieflix
+//
+//  Created by Isnard Silva on 06/11/20.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let releaseDate: String
+    let overview: String
+    let posterPath: String
+}

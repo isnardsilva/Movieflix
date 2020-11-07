977469904ddd25102076162426dae6c070003d82
Isnard Silva <isnardsilva@icloud.com>
2020-11-06 23:43:21 -0300

    Create base project

---
+++ Movieflix/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movieflix
+//
+//  Created by Isnard Silva on 06/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
